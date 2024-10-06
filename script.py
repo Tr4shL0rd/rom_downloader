e2d99c5f650e1c895b45f9e822fd23af979a3912
Tr4shL0rd <ketil04@gmail.com>
2024-10-06 16:12:32 +0200

    functional

--- script.py
+++ script.py
@@ -2,12 +2,15 @@
 import os
 from bs4 import BeautifulSoup
 import requests
+import subprocess
 from dotenv import load_dotenv
 load_dotenv()
 
 SERVER = os.getenv("SERVER_IP")
 PORT = os.getenv("SERVER_PORT")
 URL = f"http://{SERVER}:{PORT}"
+
+DIST = "roms/"
 
 def get_dirs() -> list[str]:
     """returns only dirs from SERVER"""
@@ -16,15 +19,21 @@ def get_dirs() -> list[str]:
     _dirs = [a['href'] for a in _soup.find_all('a', href=True) if a['href'].endswith('/')]
     return _dirs
 
-def get_dir_files(dir_url: str) -> list[str]:
+def get_dir_files(dir_url:str) -> list[str]:
     """Return list of files in dir"""
     _r = requests.get(f"{URL}/{dir_url}", timeout=3)
     _soup = BeautifulSoup(_r.text, 'html.parser')
     _files = [a['href'] for a in _soup.find_all('a', href=True) if not a['href'].endswith('/')]
     return _files
 
+def download_file(files:str, directory:str):
+    """asd"""
+    for _i,file in enumerate(files,0):
+        command_str = f"wget -O {DIST}{directory}{file} {URL}/{directory}{file}".split(" ")
+        subprocess.run(command_str)
+        print(command_str)
+        if _i == 1:
+            break
 
-dirs = get_dirs()
-print(dirs)
-files = get_dir_files(dirs[0])
-print(files)
+#print(get_dirs())
+download_file(files=get_dir_files(get_dirs()[3]), directory=get_dirs()[3])
